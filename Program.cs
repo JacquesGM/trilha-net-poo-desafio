b39c078446da14aabe295b20acf3ee434fd8daa7
Jacques Galdino Mascarenhas <65719772+JacquesGM@users.noreply.github.com>
2024-06-13 11:55:09 -0300

    Desafio POO realizado com sucesso

--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Testando a classe Nokia
+        Smartphone nokia = new Nokia("123456789", "Nokia Model", "111111111111111", 64);
+        Console.WriteLine($"Número: {nokia.Numero}");
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine();
+
+        // Testando a classe Iphone
+        Smartphone iphone = new Iphone("987654321", "iPhone Model", "222222222222222", 128);
+        Console.WriteLine($"Número: {iphone.Numero}");
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
